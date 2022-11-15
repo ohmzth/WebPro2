a6375ac91cbe29125e1341e85a2969601d2d862d
Mig113366 <neko113366@gmail.com>
2022-11-15 15:19:23 +0700

    Api

---
+++ WebApi/Controllers/IssueController.cs
@@ -0,0 +1,62 @@
+﻿using DemoWebAPIforstd.Data;
+using DemoWebAPIforstd.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoWebAPIforstd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssueController : ControllerBase
+    {
+        private readonly IssueDbContext _context;
+        public IssueController(IssueDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Issue>> Get()
+        {
+            return await _context.Issues.ToListAsync();
+        }
+        [HttpGet("id")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get(int id)
+        {
+            var issue = await _context.Issues.FindAsync(id);
+            return issue == null ? NotFound() : Ok(issue);
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(Issue issue)
+        {
+            await _context.Issues.AddAsync(issue);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = issue.Id }, issue);
+
+        }
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update(int id, Issue issue)
+        {
+            if (id != issue.Id) return BadRequest();
+            _context.Entry(issue).State = EntityState.Modified; 
+            await _context.SaveChangesAsync();
+            return NotFound();
+        }
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Dalete(int id)
+        {
+            var issue = await _context.Issues.FindAsync(id);
+            if(issue == null) return NotFound();
+
+            _context.Issues.Remove(issue);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
