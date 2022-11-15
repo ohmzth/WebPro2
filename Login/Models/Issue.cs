93a6619ced3a4645f902ec84719f8196507cd8f2
ohmzth <63011212023@msu.ac.th>
2022-11-15 17:25:05 +0700

    หน้า index +crud

--- Login/Models/Issue.cs
+++ Login/Models/Issue.cs
@@ -2,10 +2,16 @@
 {
     public class Issue
     {
-        public int Id { get; set; }
+      /*  public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+      */
+        public int Id { get; set; }
+      
+        public string Name { get; set; }
+        public string score { get; set; }
+        public DateTime Created { get; set; }
 
     }
 }
